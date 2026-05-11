a26fcec8d1d9a34027702e7c194a4f61fa3d4e70
Santiago Torres <santiago.torres468@pascualbravo.edu.co>
2026-05-11 12:27:02 -0500

    correciones administrativo y profesores

--- app/Livewire/RegistrarAsistencia.php
+++ app/Livewire/RegistrarAsistencia.php
@@ -3,9 +3,29 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Seguimiento;
 
 class RegistrarAsistencia extends Component
 {
+    public int $horarioId;
+    public int $estudianteId;
+    public $asistenciaRegistrada = false;
+
+    public function registrar()
+    {
+        // Guardamos el registro en la base de datos
+        Seguimiento::create([
+            'estudiante_id' => $this->estudianteId,
+            'horario_id' => $this->horarioId,
+            'fecha' => now()->toDateString(), // Fecha actual
+            'hora_registro' => now()->toTimeString(), // Hora exacta del clic
+            'estado' => 'Asistió'
+        ]);
+
+        // Cambiamos el estado para actualizar el botón en pantalla
+        $this->asistenciaRegistrada = true;
+    }
+
     public function render()
     {
         return view('livewire.registrar-asistencia');
